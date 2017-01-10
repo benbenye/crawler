5536e28516b3d2bb16c664af01d9c681c7f518a2
benbenye <1203846281@qq.com>
2017-01-10 17:35:22 +0800

    add app.js

--- app.js
+++ app.js
@@ -59,11 +59,17 @@ async function main(){
 		getList()
 			.then((list)=>{
 				result.arr = tools.dealListHtml(list.text);
-				console.log('------------总共', result.arr.length, '章节--------------');
-				tools.writeLogJson(pathFile, {last:[0,0],allList:result.arr})
-				(async ()=>{
-					for(let i = 0, l = result.arr.length; i < l; ++i) {
-						let picList = await tools.makeReq({
+				console.log('------------总共', result.arr.length, '章节--------------');				
+			})
+			.then(()=>{
+				return writeFileAsync(pathFile, JSON.stringify({last:[0,0],allList:result.arr}))
+			})
+			.then(()=>{
+				console.log('write ', pathFile, 'ok');
+				let promises = Promise.resovle();
+				for(let i = 0, l = result.arr.length; i < l; ++i) {
+					promises.then(()=>{
+						return tools.makeReq({
 							url: 'http://www.pufei.net' + result.arr[i].href,
 							Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9',
 							cookie:'__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
@@ -72,55 +78,57 @@ async function main(){
 							isCharset:false,
 							cb:function(err){console.log("第"+result.arr[i].title+""+err.message)}
 						});
-
+					})
+					.then((picList)=>{
 						if(!picList) continue;
 						result.arr[i].pic = tools.dealPicListHtml(picList.text)
 						tools.writeLogJson(pathFile, {last:[0,0],allList:result.arr})
 
 						console.log('------获取', result.arr[i].title,'的图片列表，共',result.arr[i].pic.length,'张-----------');
 
+					})
 
-						for (let j = 1, jl = result.arr[i].pic.length; j <= jl; ++j) {
-							let _pic = await tools.makeReq({
-								isCharset:false,
-								Accept:'image/webp,*/*;q=0.8',
-								url: 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j],
-								cookie:'_cfduid=dc43da4fb68265e15bb28840d3550a9c31480774326',
-								host:'ziniao.zgkouqiang.cn',
-								referer:'http://www.pufei.net'+result.arr[i].href,
-								cb:function(err){console.log('图片：'+err.message)}
-							});
+							
 
-							if(!_pic) continue;
-							let path = _path.join('imgfile' , result.arr[i].title);
-							let imgPath = _path.join(path, j + '.jpg');
+										for (let j = 1, jl = result.arr[i].pic.length; j <= jl; ++j) {
+											let _pic = await tools.makeReq({
+												isCharset:false,
+												Accept:'image/webp,*/*;q=0.8',
+												url: 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j],
+												cookie:'_cfduid=dc43da4fb68265e15bb28840d3550a9c31480774326',
+												host:'ziniao.zgkouqiang.cn',
+												referer:'http://www.pufei.net'+result.arr[i].href,
+												cb:function(err){console.log('图片：'+err.message)}
+											});
 
-							console.log('write start:', 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j])
-							let finishSave = await writeFileAsync(imgPath, _pic.body)
-							.then(function () {
-								console.log('write OK:', imgPath)
-							})
-							.catch(function () {
-								console.log('write err:', imgPath)
-								mkdirAsync(path)
-								.then(function () {
-									return writeFileAsync(imgPath, _pic.body)
-								})
-								.then(function () {
-									console.log('write file retry finished:', imgPath)
-								})
-								.catch(function (err) {
-									console.log('retry err', err)
-									throw  err;
-								});
-							});
+											if(!_pic) continue;
+											let path = _path.join('imgfile' , result.arr[i].title);
+											let imgPath = _path.join(path, j + '.jpg');
 
-						tools.writeLogJson(pathFile, {last:[i,j],allList:result.arr})
-						}
-					}
-				})
+											console.log('write start:', 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j])
+											let finishSave = await writeFileAsync(imgPath, _pic.body)
+											.then(function () {
+												console.log('write OK:', imgPath)
+											})
+											.catch(function () {
+												console.log('write err:', imgPath)
+												mkdirAsync(path)
+												.then(function () {
+													return writeFileAsync(imgPath, _pic.body)
+												})
+												.then(function () {
+													console.log('write file retry finished:', imgPath)
+												})
+												.catch(function (err) {
+													console.log('retry err', err)
+													throw  err;
+												});
+											});
+
+										tools.writeLogJson(pathFile, {last:[i,j],allList:result.arr})
+										}
+									}
 			})
 	}
 }
 main().catch((err)=>{console.log(err)})
-
