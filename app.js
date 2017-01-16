f696632b8861275002ab5e99b4d5c39ba5a2fdb1
benbenye <1203846281@qq.com>
2017-01-16 20:55:53 +0800

    change to async await

--- app.js
+++ app.js
@@ -1,131 +1,129 @@
-let fs = require('fs');
-let _path = require("path");
-let tools = require('./tools');
+const fs = require('fs');
+const _path = require("path");
+const tools = require('./tools');
 
 const writeFileAsync = tools.promisify(fs.writeFile);
 const mkdirAsync = tools.promisify(fs.mkdir);
 const appendFileAsync = tools.promisify(fs.appendFile);
 const readFileAsync = tools.promisify(fs.readFile);
+
 const style = process.argv[2];
 const pathFile = process.argv[3];
-let result = {success: true,arr:[]};
-let getList = function(){
-	'use strict'
-	return tools.makeReq({
-		url: 'http://www.pufei.net/manhua/20/',
-		Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9',
-		cookie:'__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
-		host:'www.pufei.net',
-		referer:'http://www.pufei.net/manhua/',
-		isCharset:true,
-		cb:function(err){console.log('获取章节：'+err.message)}
-	});
-};
+let result = {success: true, arr: []};
 
-if(style == 'getLastStatus' || style == 'gls'){
-	// 获取上次抓取进度
-	readFileAsync(pathFile)
-		.then(function(data){
-			let objData = JSON.parse(data.toString());
-			console.log(objData.last)
-		})
-		.catch(function(err){
-			console.log(err)
-		})	
-	return true
+let getList = function () {
+  'use strict'
+  return tools.makeReq({
+    url: 'http://www.pufei.net/manhua/20/',
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9',
+    cookie: '__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
+    host: 'www.pufei.net',
+    referer: 'http://www.pufei.net/manhua/',
+    isCharset: true,
+  })
+};
+let getPicList = function (info) {
+  return tools.makeReq({
+    url: 'http://www.pufei.net' + info.href,
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9',
+    cookie: '__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
+    host: 'www.pufei.net',
+    referer: 'http://www.pufei.net/manhua/20',
+    isCharset: false
+  })
+}
+let getPicReq = function (href, refererHref) {
+  return tools.makeReq({
+    isCharset: false,
+    Accept: 'image/webp,*/*;q=0.8',
+    url: 'http://ziniao.zgkouqiang.cn/' + href,
+    cookie: '_cfduid=dc43da4fb68265e15bb28840d3550a9c31480774326',
+    host: 'ziniao.zgkouqiang.cn',
+    referer: 'http://www.pufei.net' + refererHref,
+  })
+}
+if (style == 'getLastStatus' || style == 'gls') {
+  // 获取上次抓取进度
+  readFileAsync(pathFile)
+    .then(function (data) {
+      let objData = JSON.parse(data.toString());
+      console.log(objData.last)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  return true
 }
 
-	// 检查更新
-	if(style == 'checkUpdate'){
-		getList().then((list)=>{
-			readFileAsync(pathFile)
-				.then((data)=>{
-				let objData = JSON.parse(data.toString());
-					console.log(
-						'本地进度:',
-						objData.allList[0].title,
-						'\n抓取进度:',
-						tools.dealListHtml(list.text)[0].title
-					)
-				})
-				.catch((err)=>{
-					console.log(err)
-				})
-		}).catch((err)=>{console.log(err)})
-	}else{
-		// 主要抓取程序
-		getList()
-			.then((list)=>{
-				result.arr = tools.dealListHtml(list.text);
-				console.log('------------总共', result.arr.length, '章节--------------');				
-			})
-			.then(()=>{
-				return writeFileAsync(pathFile, JSON.stringify({last:[0,0],allList:result.arr}))
-			})
-			.then(()=>{
-				console.log('write ', pathFile, 'ok');
-				let promises = Promise.resolve();
-				for(let i = 0, l = result.arr.length; i < l; ++i) {
-					tools.makeReq({
-						url: 'http://www.pufei.net' + result.arr[i].href,
-						Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9',
-						cookie:'__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
-						host:'www.pufei.net',
-						referer:'http://www.pufei.net/manhua/20',
-						isCharset:false,
-						cb:function(err){console.log("第"+result.arr[i].title+""+err.message)}
-					})
-					.then((picList)=>{
-						if(!picList) return null;
-						return result.arr[i].pic = tools.dealPicListHtml(picList.text)
+// 检查更新
+if (style == 'checkUpdate') {
+  getList().then((list)=> {
+    readFileAsync(pathFile)
+      .then((data)=> {
+        let objData = JSON.parse(data.toString());
+        console.log(
+          '本地进度:',
+          objData.allList[0].title,
+          '\n抓取进度:',
+          tools.dealListHtml(list.text)[0].title
+        )
+      })
+      .catch((err)=> {
+        console.log(err)
+      })
+  }).catch((err)=> {
+    console.log(err)
+  })
+  return;
+}
 
-						console.log('------获取', result.arr[i].title,'的图片列表，共',result.arr[i].pic.length,'张-----------');
+// 主要抓取程序
+(async()=> {
+  try{
+    let list = await getList();
+    result.arr = tools.dealListHtml(list.text);
+    console.log('------------总共', result.arr.length, '章节--------------');
+    await writeFileAsync(pathFile, JSON.stringify({last: [0, 0], allList: result.arr}))
+    console.log('write ', pathFile, 'ok');
+    result.arr.forEach(async(e, i) => {
+      try {
+        let picList = await getPicList(e);
+        e.pic = tools.dealPicListHtml(picList.text);
+        console.log('------获取', e.title, '的图片列表，共', e.pic.length, '张-----------');
+        await writeFileAsync(pathFile, JSON.stringify({last: [0, 0], allList: result.arr}))
 
-					})
-					.then((text)=>{
-						return writeFileAsync(pathFile, JSON.stringify({last:[0,0],allList:result.arr}))
-					})
-					.then(()=>{
-						for (let j = 1, jl = result.arr[i].pic.length; j <= jl; ++j) {
-							tools.makeReq({
-								isCharset:false,
-								Accept:'image/webp,*/*;q=0.8',
-								url: 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j],
-								cookie:'_cfduid=dc43da4fb68265e15bb28840d3550a9c31480774326',
-								host:'ziniao.zgkouqiang.cn',
-								referer:'http://www.pufei.net'+result.arr[i].href,
-								cb:function(err){console.log('图片：'+err.message)}
-							})
-							.then((_pic)=>{
-								if(!_pic) return null;
+        e.pic.forEach(async(picHref, j) => {
+          if (!picHref) return;
+          try {
+            let picReq = await getPicReq(picHref, e.href);
+            if (!picReq) return new Error('not image');
 
-								let path = _path.join('imgfile' , result.arr[i].title);
-								let imgPath = _path.join(path, j + '.jpg');
+            let path = _path.join('imgfile2', e.title);
+            let imgPath = _path.join(path, j + '.jpg');
+            console.log('write start:', 'http://ziniao.zgkouqiang.cn/' + e.pic[j])
+            try {
+              await writeFileAsync(imgPath, picReq.body)
+              console.log('write OK:', imgPath)
+            }
+            catch (err) {
+              await mkdirAsync(path);
+              await writeFileAsync(imgPath, picReq.body);
+              console.log('write file retry finished:', imgPath)
+            }
+            await writeFileAsync(pathFile, JSON.stringify({last: [i, j], allList: result.arr}))
+          }
+          catch(err){
+              console.error('图片：' + err.message)
+          }
+        })
+      }
+      catch (err) {
+        console.log("第" + e.title + "" + err.message)
+      }
+    })
+  }
+  catch (err){
+   console.log('获取章节:', err.message);
+  }
 
-								console.log('write start:', 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j])
-								return writeFileAsync(imgPath, _pic.body)
-								.then(function () {
-									console.log('write OK:', imgPath)
-								})
-								.catch(function () {
-									console.log('write err:', imgPath)
-									mkdirAsync(path)
-									.then(function () {
-										return writeFileAsync(imgPath, _pic.body)
-									})
-									.then(function () {
-										console.log('write file retry finished:', imgPath)
-									})
-									.catch(function (err) {
-										console.log('retry err', err)
-										throw  err;
-									});
-								});
-
-							})
-						}
-					})		
-			}
-		})
-		.then(()=>{console.log('finished')})
-	}
+})()
--- tools.js
+++ tools.js
@@ -1,75 +1,76 @@
-
-let charset = require('superagent-charset');
-let request = require('superagent');
-let cheerio = require('cheerio');
-let fs = require('fs')
-let _path = require('path')
+const charset = require('superagent-charset');
+const request = require('superagent');
+const cheerio = require('cheerio');
+const fs = require('fs')
+const _path = require('path')
 charset(request);
 
-let tools = {
-	promisify : function (func, option = {}) {
-								return (...arg) => new Promise((resolve, reject) => {
-									return func.apply(option.context, [...arg, (err, ...data) => {
-										if (err) {
-											return reject(err);
-										}
-										if (option.multiArg) {
-											return resolve(data)
-										}
-										return resolve(data[0]);
-									}]);
-								})
-							},
-	makeReq : function(option){
-							let _req =request.get(option.url)
-							.set('Accept',option.Accept)
-							.set('Accept-Encoding','gzip, deflate')
-							.set('Accept-Language','zh-CN,zh;q=0.8,en;q=0.6')
-							.set('Cache-Control','no-cache')
-							.set('Connection','keep-alive')
-							.set('Cookie',option.cookie)
-							.set('Host',option.host)
-							.set('Pragma','no-cache')
-							.set('Referer',option.referer)
-							.set('Upgrade-Insecure-Requests','1')
-							.set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36')
-							if(option.isCharset){
-								return _req.charset('gbk')
-								.catch(option.cb)
-							}else{
-								return _req.catch(option.cb)
-							}
-	},
-	//	获取章节列表,
-	dealListHtml : function(text){
-									let $ = cheerio.load(text)
-									let _dom = $('#play_0')
-									let info = [];
-									_dom.find('a').each(function (i, ele) {
-										info[i] = {};
-										info[i].href = $(this).attr('href')
-										info[i].title = $(this).attr('title')
-									});
-									return info;
-	},
-	// 获取章节图片列表,
-	dealPicListHtml : function(text){
-										var p = text.match(/function base64decode.*/)[0];
-										var s = {};
-										p = p.replace('function base64decode', 's.base64decode = function')
-										eval(p);
-										let photosr = new Array();
-										let base64decode = s.base64decode;
-										let _paced = text.match(/packed=.*slice\(\d\)\)\)\;/)[0];
-										_paced = _paced.replace('packed=', 'let packed=')
-										eval(_paced);
-										return photosr;
-	},
-	writeLogJson : function(path,content){
-										const writeFileAsync = this.promisify(fs.writeFile);
-										writeFileAsync(_path.join(path), JSON.stringify(content))
-										.then(function(){console.log('write ', path, 'ok')})
-										.catch(function(){console.log('write ', path, 'fail')})
-	}							
+exports.promisify = function (func, option = {}) {
+  return (...arg) => new Promise((resolve, reject) => {
+    return func.apply(option.context, [...arg, (err, ...data) => {
+      if (err) {
+        return reject(err);
+      }
+      if (option.multiArg) {
+        return resolve(data)
+      }
+      return resolve(data[0]);
+    }]);
+  })
+};
+
+//	获取章节列表,
+exports.dealListHtml = function (text) {
+  let $ = cheerio.load(text)
+  let _dom = $('#play_0')
+  let info = [];
+  _dom.find('a').each(function (i, ele) {
+    info[i] = {};
+    info[i].href = $(this).attr('href')
+    info[i].title = $(this).attr('title')
+  });
+  return info;
 }
-module.exports = tools;
+// 获取章节图片列表,
+exports.dealPicListHtml = function (text) {
+  var p = text.match(/function base64decode.*/)[0];
+  var s = {};
+  p = p.replace('function base64decode', 's.base64decode = function')
+  eval(p);
+  let photosr = new Array();
+  let base64decode = s.base64decode;
+  let _paced = text.match(/packed=.*slice\(\d\)\)\)\;/)[0];
+  _paced = _paced.replace('packed=', 'let packed=')
+  eval(_paced);
+  return photosr;
+};
+
+exports.writeLogJson = function (path, content) {
+  const writeFileAsync = this.promisify(fs.writeFile);
+  writeFileAsync(_path.join(path), JSON.stringify(content))
+    .then(function () {
+      console.log('write ', path, 'ok')
+    })
+    .catch(function () {
+      console.log('write ', path, 'fail')
+    })
+};
+
+exports.makeReq = function (option) {
+  let _req = request.get(option.url)
+    .set('Accept', option.Accept)
+    .set('Accept-Encoding', 'gzip, deflate')
+    .set('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6')
+    .set('Cache-Control', 'no-cache')
+    .set('Connection', 'keep-alive')
+    .set('Cookie', option.cookie)
+    .set('Host', option.host)
+    .set('Pragma', 'no-cache')
+    .set('Referer', option.referer)
+    .set('Upgrade-Insecure-Requests', '1')
+    .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36')
+  if (option.isCharset) {
+    _req.charset('gbk');
+  }
+  return _req;
+}
